3ff724e08d7beecf54e56b2b7a15f7f5a2365cec
Vladimir Shvets <uladzimir.shvets@gmail.com>
2023-02-01 17:27:11 +0300

    Tasks 155A, 230A

---
+++ Task_155A/Program.cs
@@ -0,0 +1,32 @@
+﻿/* 
+ * Task 155A
+ * 
+ * https://codeforces.com/problemset/problem/155/A 
+ */
+
+// Read the number of competitions and their results.
+int numberOfCompetitions = int.Parse(Console.ReadLine());
+string input = Console.ReadLine();
+string[] separatedInput = input.Split(' ');
+int[] competitionResults = Array.ConvertAll(separatedInput, r => int.Parse(r));
+
+// Calculate the number of amazing results.
+int absoluteMinimum = competitionResults[0];
+int absoluteMaximum = competitionResults[0];
+int numberOfAmazingResults = 0;
+for (int i = 1; i < numberOfCompetitions; i++)
+{
+    if (competitionResults[i] < absoluteMinimum)
+    {
+        absoluteMinimum = competitionResults[i];
+        numberOfAmazingResults++;
+    }
+    else if (competitionResults[i] > absoluteMaximum)
+    {
+        absoluteMaximum = competitionResults[i];
+        numberOfAmazingResults++;
+    }
+}
+
+// Write the number of amazing results.
+Console.WriteLine(numberOfAmazingResults);
---
+++ Task_230A/Program.cs
@@ -0,0 +1,41 @@
+﻿/* 
+ * Task 230A
+ * 
+ * https://codeforces.com/problemset/problem/230/A 
+ */
+
+// Read the number of dragons and initial value of power.
+string input = Console.ReadLine();
+string[] separatedInput = input.Split(' ');
+int powerValue = int.Parse(separatedInput[0]);
+int numberOfDragons = int.Parse(separatedInput[1]);
+
+// Read the power of each dragon and the bonus in case of win.
+var competitors = new List<KeyValuePair<int, int>>();
+for (int i = 0; i < numberOfDragons; i++)
+{
+    input = Console.ReadLine();
+    separatedInput = input.Split(' ');
+    int dragonPower = int.Parse(separatedInput[0]);
+    int winBonus = int.Parse(separatedInput[1]);
+    competitors.Add(KeyValuePair.Create(dragonPower, winBonus));
+}
+
+// Fight with dragons one by one ordered by dragon power.
+foreach (var competitor in competitors.OrderBy(c => c.Key))
+{
+    if (powerValue > competitor.Key)
+    {
+        // Enough power, hero wins the dragon.
+        powerValue += competitor.Value;
+    }
+    else
+    {
+        // Not enough power, hero loses.
+        powerValue = 0;
+        break;
+    }
+}
+
+// Write the result.
+Console.WriteLine(powerValue > 0 ? "YES" : "NO");
