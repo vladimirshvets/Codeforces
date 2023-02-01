d65f9130635e7a31e4b6c20e1abd67f1ae3c597c
Vladimir Shvets <uladzimir.shvets@gmail.com>
2023-02-01 21:45:33 +0300

    Task 1778A, [Unresolved] 1778C

---
+++ Task_1778A/Program.cs
@@ -0,0 +1,52 @@
+﻿/* 
+ * Task 1778A
+ * 
+ * https://codeforces.com/problemset/problem/1778/A
+ */
+
+// Read the number of tests.
+int numberOfTests = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfTests; i++)
+{
+    // Read the sequence.
+    int sequenceLength = int.Parse(Console.ReadLine());
+    string input = Console.ReadLine();
+    string[] separatedInput = input.Split(' ');
+    int[] sequence = Array.ConvertAll(separatedInput, v => int.Parse(v));
+
+    // Calculate the summ of sequence and
+    // amount of positive and negative elements.
+    bool hasTwoNegativeInARow = false;
+    int summOfValues = sequence[0];
+    int numberOfPositive = sequence[0] > 0 ? 1 : 0;
+    for (int j = 1; j < sequenceLength; j++)
+    {
+        if (sequence[j] > 0)
+        {
+            numberOfPositive++;
+        }
+        else if (sequence[j - 1] < 0)
+        {
+            hasTwoNegativeInARow = true;
+        }
+        summOfValues += sequence[j];
+    }
+
+    // Write the result.
+    if (hasTwoNegativeInARow)
+    {
+        // Update "-1 -1" to "1 1" if possible.
+        Console.WriteLine(summOfValues + 4);
+    }
+    else if (numberOfPositive == sequenceLength)
+    {
+        // Positive values only, have to update "1 1" to "-1 -1".
+        Console.WriteLine(summOfValues - 4);
+    }
+    else
+    {
+        // Update "-1 1" to "1 -1" or vice versa to get the same summ.
+        Console.WriteLine(summOfValues);
+    }
+}
---
+++ Task_1778C/Program.cs
@@ -0,0 +1,131 @@
+﻿/* 
+ * Task 1778C
+ * 
+ * https://codeforces.com/problemset/problem/1778/C
+ * 
+ * NOTE: Works too slow to pass tests. Requires optimization or rewriting.
+ */
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Read the number of tests.
+        int numberOfTests = int.Parse(Console.ReadLine());
+        for (int i = 0; i < numberOfTests; i++)
+        {
+            // Read the word length and max size of multitude.
+            string input = Console.ReadLine();
+            string[] separatedInput = input.Split(' ');
+            int wordLength = int.Parse(separatedInput[0]);
+            int multitudeMaxSize = int.Parse(separatedInput[1]);
+            string sourceWord = Console.ReadLine();
+            string finalWord = Console.ReadLine();
+
+            // Create a list of unique letters from the source word
+            // that are different from letters on corresponding positions
+            // in final word.
+            List<char> uniqueChars = new();
+            for (int j = 0; j < sourceWord.Length; j++)
+            {
+                if (!sourceWord[j].Equals(finalWord[j]) &&
+                    !uniqueChars.Contains(sourceWord[j]))
+                {
+                    uniqueChars.Add(sourceWord[j]);
+                }
+            }
+
+            int numberOfPairs = 0;
+
+            // Generate possible subsets of letters.
+            foreach (var iteration in GetBinaryEnumerator(uniqueChars.Count()))
+            {
+                // Use only iterations of multitudeMaxSize length.
+                if (iteration.Where(i => i.Equals(true)).Count() !=
+                    Math.Min(multitudeMaxSize, uniqueChars.Count()))
+                {
+                    continue;
+                }
+
+                // Generate a list of letters that can be changed
+                // on this iteration.
+                List<char> currentUniqueChars = new();
+                for (int c = 0; c < iteration.Count(); c++)
+                {
+                    if (iteration[c])
+                    {
+                        currentUniqueChars.Add(uniqueChars[c]);
+                    }
+                }
+
+                // Produce subwords and check if they contain letters
+                // that can be changed.
+                int currentNumberOfPairs = 0;
+                for (int j = 0; j < wordLength; j++)
+                {
+                    for (int k = j; k < wordLength; k++)
+                    {
+                        bool canModifySubword = true;
+                        string sourceSubword = sourceWord.Substring(j, k - j + 1);
+                        string finalSubword = finalWord.Substring(j, k - j + 1);
+
+                        for (int l = 0; l < sourceSubword.Length; l++)
+                        {
+                            if (!currentUniqueChars.Contains(sourceSubword[l]) &&
+                                !sourceSubword[l].Equals(finalSubword[l]))
+                            {
+                                canModifySubword = false;
+                                break;
+                            }
+                        }
+
+                        if (canModifySubword)
+                        {
+                            currentNumberOfPairs++;
+                            // Console.WriteLine(sourceSubword);
+                        }
+                    }
+                }
+
+                if (currentNumberOfPairs > numberOfPairs)
+                {
+                    numberOfPairs = currentNumberOfPairs;
+                }
+            }
+
+            // Write the result for each test.
+            Console.WriteLine(numberOfPairs);
+        }
+    }
+
+    /// <summary>
+    /// Binary combination generator.
+    /// </summary>
+    /// <param name="numberOfBits">Number of bits</param>
+    /// <returns>All bit combination iterations.</returns>
+    private static IEnumerable<bool[]> GetBinaryEnumerator(int numberOfBits)
+    {
+        var binaryCombination = new bool[numberOfBits];
+
+        while (true)
+        {
+            yield return binaryCombination;
+
+            int lastZeroBitPosition =
+                Array.LastIndexOf(binaryCombination, false);
+
+            // All bits are 1, no further iterations required.
+            if (lastZeroBitPosition < 0)
+            {
+                yield break;
+            }
+
+            // Produce next iteration.
+            binaryCombination[lastZeroBitPosition] = true;
+            for (int i = lastZeroBitPosition + 1; i < numberOfBits; i++)
+            {
+                binaryCombination[i] = false;
+            }
+        }
+    }
+}
