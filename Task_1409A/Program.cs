9f2d64c0b0ac854411ff44fdba48ff6e09b4b300
Vladimir Shvets <uladzimir.shvets@gmail.com>
2023-02-07 19:50:12 +0300

    Tasks 230B, 1409A

---
+++ Task_1409A/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 
+ * Task 1409A
+ * 
+ * https://codeforces.com/problemset/problem/1409/A
+ */
+
+// Read number of tests.
+int numberOfTests = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfTests; i++)
+{
+    // Read two numbers.
+    string input = Console.ReadLine();
+    string[] separatedInput = input.Split(' ');
+    int first = int.Parse(separatedInput[0]);
+    int second = int.Parse(separatedInput[1]);
+
+    // Find the difference and cover it with summands of tens.
+    int difference = Math.Max(first, second) - Math.Min(first, second);
+    int result = difference / 10;
+
+    // If there is still some difference, cover it with one summand of 1..9.
+    if (difference % 10 > 0)
+    {
+        result++;
+    }
+
+    // Write result.
+    Console.WriteLine(result);
+}
---
+++ Task_230B/Program.cs
@@ -0,0 +1,78 @@
+﻿/* 
+ * Task 230B
+ * 
+ * https://codeforces.com/problemset/problem/230/B
+ */
+
+internal class Program
+{
+    private static readonly long s_maxPrimeValue = 1000000;
+
+    private static void Main(string[] args)
+    {
+        // Read the number of tests and test values themselves.
+        int numberOfTests = int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+        string[] separatedInput = input.Split(' ');
+
+#if DEBUG
+        var watch = new System.Diagnostics.Stopwatch();
+        watch.Start();
+#endif
+
+        // Note: square of a prime number can meet the requirements only.
+        // So we need to collect all prime numbers up to 10^6
+        // and retrieve their squares then.
+        IEnumerable<long> primeSquares = GetPrimeNumberSquares(s_maxPrimeValue);
+
+        // Proceed with each test.
+        for (int i = 0; i < numberOfTests; i++)
+        {
+            // Parse the positive number (up to 10^12).
+            long value = long.Parse(separatedInput[i]);
+
+            // Determine whether the specified value is T-Prime or not.
+            Console.WriteLine(primeSquares.Contains(value) ? "YES" : "NO"); 
+        }
+
+#if DEBUG
+        watch.Stop();
+        Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+#endif
+    }
+
+    /// <summary>
+    /// Produces the primes' squares. 
+    /// </summary>
+    /// <param name="maxPrime">Maximum prime number value</param>
+    /// <returns>The collection of squares of prime numbers.</returns>
+    private static IEnumerable<long> GetPrimeNumberSquares(long maxPrime)
+    {
+        // Using the "Sieve of Eratosthenes" approach.
+        List<long> primes = new();
+        bool[] sieve = new bool[maxPrime + 1];
+
+        for (long i = 2; i * i <= maxPrime; i++)
+        {
+            if (!sieve[i])
+            {
+                for (long j = i * i; j <= maxPrime; j += i)
+                {
+                    sieve[j] = true;
+                }
+            }
+        }
+
+        // Produce a list of squares based on prime numbers.
+        HashSet<long> primeSquares = new();
+        for (long i = 2; i <= maxPrime; i++)
+        {
+            if (!sieve[i])
+            {
+                primeSquares.Add(i * i);
+            }
+        }
+
+        return primeSquares;
+    }
+}
