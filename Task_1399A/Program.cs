8addc54d1f5905b5543bb0667be07b587a8c252c
Vladimir Shvets <uladzimir.shvets@gmail.com>
2023-02-10 15:24:59 +0300

    Tasks 189A, 1399A

---
+++ Task_1399A/Program.cs
@@ -0,0 +1,26 @@
+﻿/* 
+ * Task 1399A
+ * 
+ * https://codeforces.com/problemset/problem/1399/A 
+ */
+
+int numberOfTests = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfTests; i++)
+{
+    int numberOfValues = int.Parse(Console.ReadLine());
+    int[] values =
+        Array.ConvertAll(Console.ReadLine().Split(' '), v => int.Parse(v));
+    Array.Sort(values);
+    bool canTransform = true;
+    for (int j = 0; j < numberOfValues - 1; j++)
+    {
+        if (Math.Abs(values[j] - values[j + 1]) > 1)
+        {
+            canTransform = false;
+            break;
+        }
+    }
+
+    Console.WriteLine(canTransform ? "YES" : "NO");
+}
---
+++ Task_189A/Program.cs
@@ -0,0 +1,46 @@
+﻿/* 
+ * Task 189A
+ * 
+ * https://codeforces.com/problemset/problem/189/A 
+ */
+
+// Read input values.
+int[] input = Array.ConvertAll(
+    Console.ReadLine().Split(' '), v => int.Parse(v));
+
+int ribbonLength = input[0];
+int[] pieces = input.Skip(1).ToArray();
+int maxNumberOfPieces = 0;
+
+// If ai + bj + ck = length,
+// then the solution should be Max[i + j + k].
+for (int i = 0; i < ribbonLength / pieces[0] + 1; i++)
+{
+    for (int j = 0; j < ribbonLength / pieces[1] + 1; j++)
+    {
+        int numberOfPieces = 0;
+        int thirdPartLength = ribbonLength - pieces[0] * i - pieces[1] * j;
+        switch (thirdPartLength)
+        {
+            case < 0:
+                continue;
+
+            case 0:
+                numberOfPieces = i + j;
+                break;
+
+            case > 0 when thirdPartLength % pieces[2] == 0:
+                numberOfPieces = i + j + thirdPartLength / pieces[2];
+                break;
+        }
+
+        // Update maximum number.
+        if (numberOfPieces > maxNumberOfPieces)
+        {
+            maxNumberOfPieces = numberOfPieces;
+        }
+    }
+}
+
+// Write the result.
+Console.WriteLine(maxNumberOfPieces);
